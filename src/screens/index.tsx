4fc032cffdec25484ef071eebb292e1bcf0b5cb4
Radiant-F <radiantfadilah0@gmail.com>
2023-10-31 15:13:52 +0700

    import home

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,5 +1,6 @@
 import SplashScreen from './SplashScreen/SplashScreen';
 import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
+import Home from './Home/Home';
 
-export {SplashScreen, SignIn, SignUp};
+export {SplashScreen, SignIn, SignUp, Home};
