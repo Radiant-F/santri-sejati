f4d4c1118ed182258ef8e65e55c98cec3d2684ca
Radiant-F <radiantfadilah0@gmail.com>
2023-10-31 15:00:57 +0700

    import header & user profile

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
 import Gap from './screens/Gap';
 import FormInput from './screens/Auth/FormInput';
 import Background from './screens/Background';
+import Header from './screens/Home/Header';
+import UserProfile from './screens/Home/UserProfile';
 
-export {Gap, FormInput, Background};
+export {Gap, FormInput, Background, Header, UserProfile};
