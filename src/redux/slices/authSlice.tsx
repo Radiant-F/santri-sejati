4acf7c4dd611f0b0f5d5499335469bf782b9df1c
Radiant-F <radiantfadilah0@gmail.com>
2023-10-31 15:13:31 +0700

    update initial state & reducer

--- src/redux/slices/authSlice.tsx
+++ src/redux/slices/authSlice.tsx
@@ -1,23 +1,40 @@
 import {createSlice} from '@reduxjs/toolkit';
+import {PayloadAction} from '@reduxjs/toolkit';
 
 export interface AuthState {
   token: string;
+  user: {
+    email: string;
+    id: number;
+    name: string;
+    role: string;
+  };
 }
 
 const initialState: AuthState = {
   token: '',
+  user: {
+    email: '',
+    id: 0,
+    name: '',
+    role: '',
+  },
 };
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setToken(state, {payload}) {
-      state.token = payload;
+    setToken(state, action: PayloadAction<string>) {
+      state.token = action.payload;
+    },
+    setUser(state, {payload}) {
+      state.token = payload.token;
+      state.user = payload.user;
     },
   },
 });
 
-export const {setToken} = authSlice.actions;
+export const {setToken, setUser} = authSlice.actions;
 
 export default authSlice.reducer;
