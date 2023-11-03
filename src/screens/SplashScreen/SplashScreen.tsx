ee27e276b3ba069d00dfa4c6832a178b01814369
Radiant-F <radiantfadilah0@gmail.com>
2023-11-03 14:18:04 +0700

    update sign in request

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -2,17 +2,24 @@ import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 import EncryptedStorage from 'react-native-encrypted-storage';
+import {useSignInMutation} from '../../redux/api/authApiSlice';
 
 interface Props {
   navigation: NativeStackHeaderProps['navigation'];
 }
 
 export default function SplashScreen({navigation}: Props) {
+  const [signIn, {}] = useSignInMutation();
+
   async function refreshToken() {
     try {
-      const credential = await EncryptedStorage.getItem('user_credential');
-      if (credential) {
-        navigation.replace('SignIn');
+      const credentials = await EncryptedStorage.getItem('user_credentials');
+      if (credentials) {
+        signIn({
+          credentials: JSON.parse(credentials),
+          navigation,
+          from: 'splash',
+        });
       } else navigation.replace('SignIn');
     } catch (error) {
       console.log(error);
