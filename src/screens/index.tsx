41084746d2a0c4eef33792719417bb198e75bf83
Radiant-F <radiantfadilah0@gmail.com>
2023-11-04 11:58:16 +0700

    update import

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,5 +2,6 @@ import SplashScreen from './SplashScreen/SplashScreen';
 import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
 import Home from './Home/Home';
+import YaumiForm from './YaumiForm/YaumiForm';
 
-export {SplashScreen, SignIn, SignUp, Home};
+export {SplashScreen, SignIn, SignUp, Home, YaumiForm};
