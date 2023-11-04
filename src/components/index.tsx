e9734c8e0b53b81d836d38161f1d6f84de82266d
Radiant-F <radiantfadilah0@gmail.com>
2023-11-04 11:57:07 +0700

    update import

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,17 +1,19 @@
 import Gap from './screens/Gap';
 import FormInput from './screens/Auth/FormInput';
 import Background from './screens/Background';
-import Header from './screens/Home/Header';
+import HeaderHome from './screens/Home/Header';
 import UserProfile from './screens/Home/UserProfile';
 import FloatingButton from './screens/Home/FloatingButton';
 import YaumiGraph from './screens/Home/YaumiGraph';
 import YaumiCalendar from './screens/Home/YaumiCalendar';
+import Header from './screens/Header';
 
 export {
+  Header,
   Gap,
   FormInput,
   Background,
-  Header,
+  HeaderHome,
   UserProfile,
   FloatingButton,
   YaumiGraph,
