30471e2918af61997336838d03ae8b8e676f9ddb
Radiant-F <radiantfadilah0@gmail.com>
2023-11-07 15:30:35 +0700

    update header, add refresh control & sign out

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,21 +1,80 @@
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Alert,
+  ToastAndroid,
+  RefreshControl,
+} from 'react-native';
 import React from 'react';
 import {
   Background,
   FloatingButton,
-  HeaderHome,
   UserProfile,
   YaumiGraph,
   Gap,
   YaumiCalendar,
+  Header,
 } from '../../components';
+import {ParamListBase} from '@react-navigation/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {
+  useYaumiCalendarQuery,
+  useYaumiQuery,
+} from '../../redux/api/yaumiApiSlice';
 
-export default function Home() {
+type Props = NativeStackScreenProps<ParamListBase, 'Home'>;
+
+export default function Home({navigation}: Props) {
+  const {isLoading: loadingYaumi, refetch: refetchYaumi} = useYaumiQuery(null);
+  const {isLoading: loadingCalendar, refetch: refetchYaumiCalendar} =
+    useYaumiCalendarQuery(new Date().getMonth() + 1);
+
+  async function signOut() {
+    try {
+      await EncryptedStorage.removeItem('user_credentials');
+      navigation.replace('SignIn');
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+      ToastAndroid.show(
+        `Terjadi kesalahan: ${errorMessage}`,
+        ToastAndroid.LONG,
+      );
+      console.log('ERROR Sign Out:', error);
+    }
+  }
+
+  function confirmSignOut() {
+    Alert.alert('Keluar?', 'Sesi Anda akan berakhir.', [
+      {text: 'Batal'},
+      {text: 'Keluar', onPress: () => signOut()},
+    ]);
+  }
+
+  const loadingData = loadingYaumi || loadingCalendar;
+  function refetchData() {
+    refetchYaumiCalendar();
+    refetchYaumi();
+  }
+
   return (
     <View style={{flex: 1}}>
       <Background />
-      <ScrollView>
-        <HeaderHome />
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            onRefresh={() => refetchData()}
+            refreshing={loadingData}
+          />
+        }>
+        <Header
+          onPress={() => confirmSignOut()}
+          iconName="logout"
+          iconRotate="180deg"
+          title="Santri Sejati"
+        />
         <View style={styles.container}>
           <UserProfile />
           <Gap height={20} />
