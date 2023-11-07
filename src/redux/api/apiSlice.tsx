dad12792277b7797ffdee2d58bb82f78b8399227
Radiant-F <radiantfadilah0@gmail.com>
2023-11-07 15:28:44 +0700

    add base query with reauth

--- src/redux/api/apiSlice.tsx
+++ src/redux/api/apiSlice.tsx
@@ -1,5 +1,13 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import type {RootState} from '..';
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+import {Mutex} from 'async-mutex';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {setToken} from '../slices/authSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://dev.pondokdigital.pondokqu.id/api',
@@ -13,8 +21,52 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const mutex = new Mutex();
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock();
+
+  let result: any = await baseQuery(args, api, extraOptions);
+  const invalidToken =
+    result.data?.status == 'Authorization Token not found' ||
+    result.data?.status == 'Token is Expired' ||
+    result.data?.status == 'Token is Invalid';
+
+  if (invalidToken) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      try {
+        const credentials = await EncryptedStorage.getItem('user_credentials');
+        const {data}: any = await baseQuery(
+          {
+            url: '/login',
+            body: credentials ? JSON.parse(credentials) : null,
+            method: 'POST',
+          },
+          api,
+          extraOptions,
+        );
+
+        api.dispatch(setToken(data.token));
+        result = await baseQuery(args, api, extraOptions);
+      } catch (error) {
+        console.log('ERROR REAUTH:', error);
+      } finally {
+        release();
+      }
+    } else {
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   endpoints: builder => ({}),
   tagTypes: ['User'],
 });
