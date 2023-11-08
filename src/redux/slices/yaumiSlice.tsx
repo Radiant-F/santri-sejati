3b59743f5e2a4ed053273f7c8a03a0176f06e7ea
Radiant-F <radiantfadilah0@gmail.com>
2023-11-08 16:41:27 +0700

    update calendar initial state

--- src/redux/slices/yaumiSlice.tsx
+++ src/redux/slices/yaumiSlice.tsx
@@ -1,5 +1,33 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+function getDaysInMonth() {
+  switch (new Date().getMonth()) {
+    case 0: // January
+    case 2: // March
+    case 4: // May
+    case 6: // July
+    case 7: // August
+    case 9: // October
+    case 11: // December
+      return 31;
+    case 3: // April
+    case 5: // June
+    case 8: // September
+    case 10: // November
+      return 30;
+    case 1: // February
+      const year = new Date().getFullYear(); // Get the current year
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        // Leap year
+        return 29;
+      } else {
+        return 28;
+      }
+    default:
+      return 0; // Invalid month index
+  }
+}
+
 const initialState = {
   graph_data: {
     stw_jamaah: {
@@ -27,7 +55,7 @@ const initialState = {
       didnt: 0,
     },
   },
-  calendar: [false, false, false, false, false],
+  calendar: Array(getDaysInMonth()).fill(false),
 };
 
 const yaumiSlice = createSlice({
