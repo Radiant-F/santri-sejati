0f86d7500c073c8ec7e29cdb31ef9a3c17cf7f1e
Radiant-F <radiantfadilah0@gmail.com>
2023-11-08 16:40:53 +0700

    add mutation query

--- src/redux/api/yaumiApiSlice.tsx
+++ src/redux/api/yaumiApiSlice.tsx
@@ -1,3 +1,4 @@
+import {RootState} from '..';
 import {setCalendar, setYaumi} from '../slices/yaumiSlice';
 import {apiSlice} from './apiSlice';
 
@@ -6,6 +7,7 @@ const yaumiApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     yaumi: builder.query({
       query: () => '/yaumi-santri/dashboard',
+      providesTags: ['Yaumi'],
       async onQueryStarted(arg, {queryFulfilled, dispatch}) {
         try {
           const {data} = await queryFulfilled;
@@ -17,16 +19,50 @@ const yaumiApiSlice = apiSlice.injectEndpoints({
     }),
     yaumiCalendar: builder.query({
       query: month => `/month/calendar/${month}`,
-      async onQueryStarted(arg, {queryFulfilled, dispatch}) {
+      providesTags: ['Yaumi'],
+      async onQueryStarted(arg, {queryFulfilled, dispatch, getState}) {
         try {
           const {data} = await queryFulfilled;
-          dispatch(setCalendar(Object.values(data)));
+          const {calendar} = (getState() as RootState).yaumi;
+          // const combined = Object.values(data).forEach((value, index) => {
+          //   if (index < calendar.length) return value;
+          //   return calendar[index];
+          // });
+          // const combined = Object.values(data).map((value, index) => {
+          //   if (index < calendar.length) return value;
+          //   return calendar[index];
+          // });
+          const combined = calendar.map((value, index) => {
+            if (index < Object.values(data).length) {
+              return Object.values(data)[index];
+            }
+            return value;
+          });
+
+          dispatch(setCalendar(combined));
         } catch (error) {
           console.log('ERROR YAUMI CALENDAR:', error);
+        }
+      },
+    }),
+    yaumiForm: builder.mutation({
+      query: ({body}) => ({
+        url: '',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Yaumi'],
+      async onQueryStarted({navigation}, {dispatch, queryFulfilled}) {
+        try {
+          const {data} = await queryFulfilled;
+          console.log('SUCCESS YAUMI FORM', data);
+        } catch (error) {
+          console.log('ERROR YAUMI FORM', error);
         }
       },
     }),
   }),
 });
 
-export const {useYaumiQuery, useYaumiCalendarQuery} = yaumiApiSlice;
+export const {useYaumiQuery, useYaumiCalendarQuery, useYaumiFormMutation} =
+  yaumiApiSlice;
